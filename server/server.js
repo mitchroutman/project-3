b03c7d0c5da1978f2b868f744c7307c2fc7c5a47
Mitchell Routman <mitchell.routman@gmail.com>
2022-03-30 11:58:12 -0400

    updates to server.js - added apollo dependencies

--- server/server.js
+++ server/server.js
@@ -1,12 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const app = express();
+const db = require('./connections/config');
 const PORT = process.env.PORT || 3001;
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.listen(PORT, ()=>{
-    console.log("App is listening on: http://localhost:" + PORT)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API running on port ${PORT}`);
+    console.log(`Graphql at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+
+// app.listen(PORT, ()=>{
+//     console.log("App is listening on: http://localhost:" + PORT)
+// })
