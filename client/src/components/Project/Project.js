6c46bbb5430f2bc18f908db5569766ac40108fb2
Shifra Wagshall <shifrawagshall@gmail.com>
2022-04-04 13:56:04 -0400

    changes to help with merge conflict.

--- client/src/components/Project/Project.js
+++ client/src/components/Project/Project.js
@@ -1,9 +1,65 @@
 import React from "react";
- 
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 export default function Project() {
     return(
         <div>
-            <h1>Projects Dashboard</h1>
+        <div id='dashboard-header'>
+          <h1>Dashboard</h1>
         </div>
-    )
+        
+        <div className="card">
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
+                Timeline for Website Development
+              </Typography>
+              {/* <Typography variant="h5" component="div">
+                be{bull}nev{bull}o{bull}lent
+              </Typography> */}
+              {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography> */}
+              <Typography variant="body2">
+                Due 4/1/22
+                <br />
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </div>
+              <br></br>
+    
+        <div className="card">
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 22 }} color="text.secondary" gutterBottom>
+                    Develop new portfolio website          
+              </Typography>
+              {/* <Typography variant="h5" component="div">
+                be{bull}nev{bull}o{bull}lent
+              </Typography> */}
+              {/* <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography> */}
+              <Typography variant="body2">
+                Due 4/1/22
+                <br />
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </div>
+    
+      </div>
+    );
 }
