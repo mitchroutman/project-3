ecc2a8b9b63bf210cb2bbfeb49e0d0ee69a98191
Shifra Wagshall <shifrawagshall@gmail.com>
2022-04-04 12:59:35 -0400

    Added footer and changed some of the header styling.

---
+++ client/src/components/common/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+    <footer className="footer">
+      <div >
+      <p className='text'>
+            &copy; 2022 Agile Lake, Inc.
+        </p>
+       
+      </div>
+    
+    </footer>
+  );
+    
+  export default Footer;
